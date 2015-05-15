45df005ec491f45c238ba9a453f551246030704c
Vaibhav Bansal <vabby.bansal@gmail.com>
2015-05-16 02:32:23 +0530

    filterArray Done

--- index.php
+++ index.php
@@ -3,10 +3,37 @@
 <head>
 	<meta charset="UTF-8">
 	<title>The Orderite</title>
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
 </head>
 <body>	
-
 	
+	<div class="container">
+		<div id="controlPanel">
+			<div class='filters lf cf'>
+				<div>Filters</div>
+				<div id="_filDelegate">
+					<div><input type="checkbox" name="criteria" value="initiated"> Initiated</div>
+					<div><input type="checkbox" name="criteria" value="failed"> Failed</div>
+					<div><input type="checkbox" name="criteria" value="success"> Success</div>
+					<div><input type="checkbox" name="criteria" value="refunded"> Refunded</div>
+					<div><input type="checkbox" name="criteria" value="dropped"> Dropped</div>
+				</div>
+			</div>
+			<div class='sorting lf cf'>
+				<div>Sorting</div>
+				<div id="_sortDelegate">
+					<div><input type="radio" name="sorting" value="paymentid"> Payment Id</div>
+					<div><input type="radio" name="sorting" value="orderdate"> Order Date</div>
+					<div><input type="radio" name="sorting" value="amount"> Amount</div>
+					<!-- <div><input type="radio" name="sorting" value="refunded"> Refunded</div>
+					<div><input type="radio" name="sorting" value="dropped"> Dropped</div> -->
+				</div>
+			</div>
+		</div>
+		<div id="theOrderite">
+			
+		</div>
+	</div>
 
 	
 	<script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 var theOrderite = (function(){
 
 	var model = {
-		currentRecord:{},
+		currentRecord:{}, 				// Object with the AJAX Data
+		// paymntIdStatArr:[],
+		// amntIdStatArr:[],
+		// dateIdStatArr:[],
+		currentSeq:[],					// Index array of the current sorting order relative to the currentRecord index
+		currentFilArr:[],				// Resultant array after applying the latest filter. UI Changes to be made next
+		lastFilArr:[],					// last state of resultant array(prev value of currentFilArr). currentFilArr and lastFilArr sent to the rendering function to make changes whatever
+		tempFilter:[],					// Latest array consisting the various filters currently applied. 
+		// currentStatus:{
+		// 	filter:0,
+		// 	sort:0
+		// },
 		appCnstns:{
 			dataFileSrc:'data/FrontEndProblem1.json'
 		}
@@ -10,9 +21,22 @@ var theOrderite = (function(){
 	var controller = {
 		init:function(){
 			this.getAjaxData();
+			view.addListeners();
+			Array.prototype.deepCopy = function(){
+				return this.slice(0);
+			}
 		},
 		stringNormalizeJSON:function(xhrOut){
-			return xhrOut.replace(/'/g,"\"");
+			return xhrOut.replace(/'/g,"\"")
+			   .replace(/\\n/g, "\\n")  
+               .replace(/\\'/g, "\\'")
+               .replace(/\\"/g, '\\"')
+               .replace(/\\&/g, "\\&")
+               .replace(/\\r/g, "\\r")
+               .replace(/\\t/g, "\\t")
+               .replace(/\\b/g, "\\b")
+               .replace(/\\f/g, "\\f")
+               .replace(/[\u0000-\u0019]+/g,"");
 		},
 		getAjaxData:function(){
 			var xhr = new XMLHttpRequest();
@@ -21,7 +45,8 @@ var theOrderite = (function(){
 				if(xhr.status === 200){
 					var normalJSON = controller.stringNormalizeJSON(xhr.responseText);
 					model.currentRecord = JSON.parse(normalJSON);
-					// view.updateUIRecords(model.currentRecord);
+					view.updateUIRecords(model.currentRecord);
+					controller.updateStatData();
 					xhr = null;
 				}
 				else{
@@ -29,14 +54,114 @@ var theOrderite = (function(){
 				}
 			};
 			xhr.send();
+		},
+		updateStatData:function(){
+			var dataObj = model.currentRecord;
+			var dataObjLen = model.currentRecord.length;
+			for(var i=0; i<dataObjLen; i++)
+			{
+				// model.paymntIdStatArr[i] = dataObj[i].paymentId;
+				// model.amntIdStatArr[i] = dataObj[i].amount;
+				// model.dateIdStatArr[i] = dataObj[i].orderDate;
+				model.currentSeq[i] = i;
+				dataObj[i].paymentStatus = dataObj[i].paymentStatus.toLowerCase(); 
+			}
+		},
+		filterArr:function(criteria,add){
+			
+			var data = model.currentRecord;
+			// var len = data.length;
+			var dataSeq = model.currentSeq;
+			var len = dataSeq.length;
+			tempFilter = model.tempFilter;
+			var arr = new Array();
+
+
+			if(add)
+			{
+				tempFilter.push(criteria);
+			}
+			else
+			{
+				var index = tempFilter.indexOf(criteria);
+				if (index > -1) {
+				    tempFilter.splice(index, 1);
+				}
+			}
+			var tempFilterLen = tempFilter.length;
+
+			for(var i=0; i<len; i++)
+			{
+				for(var j=0; j<tempFilterLen; j++)
+				{
+
+					if(data[dataSeq[i]].paymentStatus === tempFilter[j])
+					{
+						arr.push(dataSeq[i]);
+					}
+				}
+			}
+			model.currentFilArr = arr;
+			this.renderUpdates();
+
+		},
+		renderUpdates:function(){
+			console.log(model.lastFilArr + " - " + model.currentFilArr)
+			model.lastFilArr = model.currentFilArr.deepCopy();
 		}
 
 	};
 
 	var view = {
-		updateUIRecords:function(){
+		updateUIRecords:function(dataObj){
+			var dataObjLen = dataObj.length;
+			var dataTable = document.getElementById("theOrderite");
+			var recordHTML = "<table id='dataTable'><tr><th>Seq</th><th>Payement Id</th><th>Order Date</th><th>Merchant Id</th><th>Customer Email</th><th>Amount</th><th>Payment Status</th></tr>";
+			for(var i=0; i<dataObjLen; i++)
+			{
+				recordHTML += "<tr><td>"+ (i+1) + "</td><td>" + dataObj[i].paymentId  + "</td><td>" + dataObj[i].orderDate + "</td><td>" + dataObj[i].merchatId + "</td><td>" + dataObj[i].customerEmail + "</td><td>" + dataObj[i].amount + "</td><td>" + dataObj[i].paymentStatus + "</td></tr>";
+			}
+			recordHTML += "</table>";
+			dataTable.innerHTML = recordHTML;
+		},
+		addListeners:function(){
+			document.getElementById("_filDelegate").addEventListener("click",this.clickHandle);
+			document.getElementById("_sortDelegate").addEventListener("click",this.clickHandle);
+
+		},
+		clickHandle:function(event){
+			var targetElm;
 			
+			if(event.target.nodeName === 'INPUT')
+			{
+				targetElm = event.target;
+			}
+			else
+			{
+				targetElm = event.target.children[0];
+				targetElm.checked = !targetElm.checked;
+			}
+
+			if(targetElm.name === "criteria")
+			{
+				if(targetElm.checked === true)
+				{
+					controller.filterArr(targetElm.value,1);
+				}
+				else
+				{
+					controller.filterArr(targetElm.value,0);
+				}
+			}
+			else
+			{
+
+			}
+		},
+		renderUpdates:function(){
+
 		}
+
 	};
 
 	controller.init();
